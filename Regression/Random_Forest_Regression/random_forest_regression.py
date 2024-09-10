a97470ebc5b4ba5e0aaf922379c6cc93cefdbb3f
altnbsmehmet <altnbsmehmet@icloud.com>
2024-09-10 09:12:12 +0300

    Revert.

---
+++ Regression/Random_Forest_Regression/random_forest_regression.py
@@ -0,0 +1,28 @@
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('Random_Forest_Regression/Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+#training the random forest regression model on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, y)
+
+#predicting a new result
+y_pred = regressor.predict([[6.5]])
+print(y_pred)
+
+#visualizing the random forest regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Random Forest Regression Prediction (for higher resolution)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
